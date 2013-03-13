178d5d5eef68af8a009a4adf24eb54100b3c84c9
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2013-03-13 19:26:00 -0400

    Print out result of compilation in JS

--- compile.ml
+++ compile.ml
@@ -3,10 +3,12 @@ open Ast
 let translate_var v =
 	"v" ^ Str.global_replace (Str.regexp "'") "_u" v
 
-let rec compile e = match e with
+let rec compile_rec e = match e with
 	| Var x -> translate_var x
-	| Application(e1, e2) -> "(" ^ compile e1 ^ "(" ^ compile e2 ^ "))"
-	| Abstraction(arg, body) -> "(function(" ^ translate_var arg ^ ") {return (" ^ compile body ^ ");})"
+	| Application(e1, e2) -> "(" ^ compile_rec e1 ^ "(" ^ compile_rec e2 ^ "))"
+	| Abstraction(arg, body) -> "(function(" ^ translate_var arg ^ ") {return (" ^ compile_rec body ^ ");})"
 	| Integer n -> string_of_int n
-	| Binop(op, e1, e2) -> "(" ^ compile e1 ^ string_of_binop op ^ compile e2 ^ ")"
-	| Unop(Print, e) -> "((function(x) {console.log(x);return x;})(" ^ compile e ^ "))"
+	| Binop(op, e1, e2) -> "(" ^ compile_rec e1 ^ string_of_binop op ^ compile_rec e2 ^ ")"
+	| Unop(Print, e) -> "((function(x) {console.log(x);return x;})(" ^ compile_rec e ^ "))"
+
+let compile e = "console.log(\"Result: \" + " ^ compile_rec e ^ ");"
