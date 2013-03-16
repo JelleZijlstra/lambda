97165ebf2422bd38d7da825f1b2ff9a08578973d
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2013-03-16 16:17:34 -0400

    Add CBN semantics (not tested yet)'

--- eval.ml
+++ eval.ml
@@ -1,13 +1,15 @@
 open Ast
 
-let get_unique_var_name =
+type semantics = CBV | CBN
+
+let get_unique_var_name : unit -> string =
 	let current = ref 0 in
 	fun () ->
 		let index = !current in
 		current := index + 1;
 		"'" ^ (string_of_int index)
 
-let rec is_free_variable var code = match code with
+let rec is_free_variable (var : string) (code : expr) : bool = match code with
 	| Var x -> var = x
 	| Application(e1, e2) -> (is_free_variable var e1) || (is_free_variable var e2)
 	| Abstraction(arg, body) -> (arg <> var) && (is_free_variable var body)
@@ -15,7 +17,7 @@ let rec is_free_variable var code = match code with
 	| Binop(_, e1, e2) -> is_free_variable var e1 || is_free_variable var e2
 	| Unop(_, e) -> is_free_variable var e
 
-let rec substitute code var replacement = match code with
+let rec substitute (code : expr) (var : string) (replacement : expr) : expr = match code with
 	| Var(x) -> if x = var then replacement else Var x
 	| Application(e1, e2) -> Application(substitute e1 var replacement, substitute e2 var replacement)
 	| Integer _ -> code
@@ -28,28 +30,33 @@ let rec substitute code var replacement = match code with
 			else let var_name = get_unique_var_name () in
 				Abstraction(var_name, substitute (substitute body arg (Var var_name)) var replacement)
 
-let rec eval e = match e with
+
+let rec eval (e : expr) (s : semantics) : expr = match e with
 	| Var x -> failwith ("Unbound variable: " ^ x)
 	| Abstraction(_, _) -> e
 	| Integer n -> Integer n
-	| Binop(op, e1, e2) -> eval_binop op e1 e2
+	| Binop(op, e1, e2) ->
+		let e1' = eval e1 s in
+		let e2' = eval e2 s in
+		(match e1', e2' with
+		| Integer n1, Integer n2 -> Integer(f_of_binop op n1 n2)
+		| Integer _, _ -> failwith("Invalid operand to binary expression: " ^ string_of_expr e2')
+		| _, _ -> failwith("Invalid operand to binary expression: " ^ string_of_expr e1'))
 	| Unop(op, e) ->
-		let e' = eval e in
+		let e' = eval e s in
 		(match e' with
 		| Integer n -> Integer(f_of_unop op n)
 		| _ -> failwith("Invalid operand to unary expression: " ^ string_of_expr e))
 	| Application(e1, e2) ->
-		let e1' = eval e1 in
-		let e2' = eval e2 in
+		let e1' = eval e1 s in
+		let e2' = match s with
+			| CBV -> eval e2 s
+			| CBN -> e2 in
 		match e1' with
 		| Abstraction(arg, body) ->
 			let substituted = substitute body arg e2' in
-			eval substituted
+			eval substituted s
 		| _ -> failwith "This expression is not a function; it cannot be applied"
-and eval_binop op e1 e2 =
-	let e1' = eval e1 in
-	let e2' = eval e2 in
-	match e1', e2' with
-	| Integer n1, Integer n2 -> Integer(f_of_binop op n1 n2)
-	| Integer _, _ -> failwith("Invalid operand to binary expression: " ^ string_of_expr e2')
-	| _, _ -> failwith("Invalid operand to binary expression: " ^ string_of_expr e1')
+
+let eval_cbv e = eval e CBV
+let eval_cbn e = eval e CBN
