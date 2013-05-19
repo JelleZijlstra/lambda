18c00bc56fd2b372c3be41e7757e2d19d58e550c
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2013-05-19 10:06:09 -0400

    Add not

--- pyscheme/library.scm
+++ pyscheme/library.scm
@@ -1,3 +1,5 @@
 ; standard library for this Scheme dialect, written in Scheme itself
 
 (define list (lambda (x . xs) (cons x xs)))
+
+(define not (lambda (arg) (if arg #f #t)))
--- pyscheme/test/uselib.scm
+++ pyscheme/test/uselib.scm
@@ -1,1 +1,5 @@
 (print (list 1 2 3))
+
+(print (not #t))
+
+(print (not #f))
