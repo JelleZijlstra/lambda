3eb1f8264b28797e3f4d49a1a78647270560b182
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2013-10-27 21:31:42 -0400

    Add a curried builtin function. It's tedious though, and I should think of a better way to do this.

--- typeinference/builtin.ml
+++ typeinference/builtin.ml
@@ -10,12 +10,19 @@ let print_f v =
     Printf.printf "%s\n" str;
     VUnit
 
+let add_strings_f s1 =
+    Ast.VBuiltin (fun s2 -> match s1, s2 with
+        | VString s1', VString s2' -> VString(s1' ^ s2')
+        | _ -> failwith("Typecheck failure"))
+
 let get_builtins () =
     let m = VarMap.empty in
     (* echo : forall a. a -> () *)
     let m = VarMap.add "echo" (TForAll(["a"], TFunction(Typevar "a", TUnit)), echo_f) m in
     (* print : forall a. a -> () *)
     let m = VarMap.add "print" (TForAll(["a"], TFunction(Typevar "a", TUnit)), print_f) m in
+    (* add_strings : string -> string -> string *)
+    let m = VarMap.add "add_strings" (TFunction(TString, TFunction(TString, TString)), add_strings_f) m in
     m
 
 let builtins = get_builtins()
