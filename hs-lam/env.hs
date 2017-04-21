c86735315e44ca6a80eb34e2baa8fccadf2a5840
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-04-21 09:13:39 -0700

    add support for builtin functions

---
+++ hs-lam/env.hs
@@ -0,0 +1,12 @@
+module Env(env) where
+
+import Ast
+import qualified Data.Map as Map
+
+printB :: Value -> EvalResult Value
+printB v = do
+    Success v $ putStrLn $ show v
+    return v
+
+env :: Map.Map String Value
+env = Map.fromList [("printX", VBuiltin printB)]
